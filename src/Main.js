a64a25d82fdc3d7dc8cf0a8d8204cf27f5008d9d
Ron Ellis <rkeiii@gmail.com>
2018-07-07 14:34:50 -0700

    my fucking fish loads

--- src/Main.js
+++ src/Main.js
@@ -7,17 +7,17 @@
 
 function MainState(){
 
-	BasicGame.Main = function (game) { this.game; this.add; this.camera; this.cache; this.input; this.load; this.math; this.sound; this.stage; this.time; this.tweens; this.state; this.world; this.particles; this.physics; this.rnd;  
+	BasicGame.Main = function (game) { this.game; this.add; this.camera; this.cache; this.input; this.load; this.math; this.sound; this.stage; this.time; this.tweens; this.state; this.world; this.particles; this.physics; this.rnd;
 	};
-	
+
 	BasicGame.Main.prototype = {
 		create: function () {
 			var middleScreen = this.game.width / 2;
-			
+
 			function menuClick(game){
 				navbar.open(this);
 			}
-			
+
 			var imgMenu = this.add.sprite(this.game.width, 0, 'menu');
 			imgMenu.anchor.set(1,0);
 			imgMenu.inputEnabled = true;
@@ -35,13 +35,13 @@ function MainState(){
 			this.add.tween(imgButton).to({ x: btnLoc.x, y: btnLoc.y }, 500, Phaser.Easing.Back.Out, true);
 			imgButton.inputEnabled = true;
 			imgButton.events.onInputDown.add(menuClick, this);
-			
-			for (var i = 0; i < 20; i++) { 
+
+			for (var i = 0; i < 20; i++) {
 				new JellyFish(this);
 			}
 			var game = BasicGame.Main;
-			
-			var navbar = new NavBar(this);
+
+			var navbar = new NavBar.NavBar(this);
 		}
 	}
 }
@@ -62,7 +62,7 @@ JellyFish = function (objectScope) {
 	}
 
 	Phaser.Sprite.call(this, game, getRandom(0, game.world.width), getRandom(0, game.world.height), 'spJellyFish');
-	this.animations.add('default');   
+	this.animations.add('default');
 	this.animations.play('default', 30, true);
 	this.animations.getAnimation('default').frame = getRandom(2, this.animations.getAnimation('default').frameTotal);
 
@@ -79,13 +79,13 @@ JellyFish.prototype.constructor = JellyFish;
 JellyFish.prototype = Object.create(Phaser.Sprite.prototype);
 
 JellyFish.prototype.update = function(){
-	
+
 	this.xSpeed = Math.cos((this.angle)/180*Math.PI) * - this.acc;
 	this.ySpeed = Math.sin((this.angle)/180*Math.PI) * - this.acc;
-	
+
 	this.x += this.xSpeed;
 	this.y += this.ySpeed;
-	
+
 	//Reset if off screen
 	function checkBounds(sprite){
 		var game = sprite.game;
@@ -106,10 +106,6 @@ JellyFish.prototype.update = function(){
 			sprite.y = worldHeight + boundsSize;
 		}
 	}
-	
+
 	checkBounds(this);
 }
-
-
-
-
