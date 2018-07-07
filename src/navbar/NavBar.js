0cbd4df7d3249e0a57e9939925ee0dea6111a96a
Jesse Wallace <jjaywallace@gmail.com>
2018-07-07 15:41:27 -0700

    suck my atom

--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -16,21 +16,21 @@ class NavBar {
 		this.pol = {
 			xtl: scope.game.width,
 			ytl: 0,
-			xtr: scope.game.width + menuWidth,
+			xtr: scope.game.width + this.menuWidth,
 			ytr: 0,
 
 			xbl: scope.game.width,
 			ybl: scope.game.height,
-			xbr: scope.game.width + menuWidth,
+			xbr: scope.game.width + this.menuWidth,
 			ybr: scope.game.height,
 		}
 
 		//Make the polygon
 		this.poly = new Phaser.Polygon([
-			new Phaser.Point(pol.xtl, pol.ytl),
-			new Phaser.Point(pol.xtr, pol.ytr),
-			new Phaser.Point(pol.xbr, pol.ybr),
-			new Phaser.Point(pol.xbl, pol.ybl)
+			new Phaser.Point(this.pol.xtl, this.pol.ytl),
+			new Phaser.Point(this.pol.xtr, this.pol.ytr),
+			new Phaser.Point(this.pol.xbr, this.pol.ybr),
+			new Phaser.Point(this.pol.xbl, this.pol.ybl)
 		]);
 
 		//Convert the polygon into a graphic element
@@ -45,41 +45,41 @@ class NavBar {
 		this.menuBackground = scope.game.add.sprite(this.menuWidth, 0);
 		this.menuBackground.addChild(this.graphics);
 		this.menuBackground.inputEnabled = true;
-		this.menuBackground.events.onInputDown.add(clickOnGraphics, this);
+		this.menuBackground.events.onInputDown.add(this.clickOnGraphics, this);
 
 		//Introduce yourself -Navbar
 		console.log('Hello! -Navbar');
 
 	}
 
-	function clickOnGraphics(){
+	clickOnGraphics(){
 		console.log('Close!');
 		this.toggle = false;
-		scope.add.tween(menuBackground).to({ x: 0, y: 0 }, 200, Phaser.Easing.Linear.Out, true);
+		scope.add.tween(this.menuBackground).to({ x: 0, y: 0 }, 200, Phaser.Easing.Linear.Out, true);
 	}
 
-	function open(scope){
+	open(scope){
 		console.log('Open!');
 		console.log(NavBar.prototype);
 		if(this.toggle == true){
 			this.toggle = false;
-			scope.add.tween(menuBackground).to({ x: -200, y: 0 }, 200, Phaser.Easing.Linear.In, true);
+			scope.add.tween(this.menuBackground).to({ x: -this.menuWidth, y: 0 }, 200, Phaser.Easing.Linear.In, true);
 		}else{
 			this.toggle = true;
-			scope.add.tween(menuBackground).to({ x: 0, y: 0 }, 200, Phaser.Easing.Linear.In, true);
+			scope.add.tween(this.menuBackground).to({ x: 0, y: 0 }, 200, Phaser.Easing.Linear.In, true);
 		}
 	}
 
-	class CreatItem {
-		constructor() {
-			var imgMenu = this.add.sprite(this.game.width, 0, img);
-			item.anchor.set(0.5,0);
-			item.inputEnabled = true;
-			item.events.onInputDown.add(clickMe, this);
-		}
-
-		function clickMe(){
-			game.state.start(state);
-		}
-	}
+	// class CreatItem {
+	// 	constructor() {
+	// 		var imgMenu = this.add.sprite(this.game.width, 0, img);
+	// 		item.anchor.set(0.5,0);
+	// 		item.inputEnabled = true;
+	// 		item.events.onInputDown.add(clickMe, this);
+	// 	}
+  //
+	// 	function clickMe(){
+	// 		game.state.start(state);
+	// 	}
+	// }
 }
